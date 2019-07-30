5887a12a13c54ea98003d9918931c829162609f1
Elinex <eline.ax@gmail.com>
2019-07-29 17:21:34 -0700

    postgres schema

--- server/postgres/postgresSchema.sql
+++ server/postgres/postgresSchema.sql
@@ -1,46 +1,46 @@
-CREATE TABLE listings (
-  id INTEGER NOT NULL PRIMARY_KEY,
-  title CHARACTER varying(60) NOT NULL,
-  location CHARACTER varying(60) NOT NULL,
-  rating NUMERIC(2) NOT NULL,
-  total_ratings INTEGER NOT NULL
-  user_id INTEGER NOT NULL REFERENCES users(id)
-)
--- To join with listings:
-  -- photos: [
-  --   ObjectId(PHOTO_ID) // all photos ID for each listing
-  -- ],
-  -- favorites: [
-  --   ObjectId(FAVORITE_ID) // all favorites ID for each listing
-  -- ]
-
-CREATE TABLE photos (
-  id INTEGER NOT NULL PRIMARY_KEY,
-  listing_id INTEGER NOT NULL REFERENCES listings(id),
-  photo_url VARCHAR NOT NULL,
-  priority INTEGER NOT NULL,
-  caption VARCHAR NOT NULL
-)
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS listings;
+DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS favorites;
 
 CREATE TABLE users (
-  id INTEGER NOT NULL PRIMARY_KEY,
-  username CHARACTER varying(30) NOT NULL,
-  facebook VARCHAR NOT NULL,
-  twitter VARCHAR NOT NULL,
-  messenger VARCHAR NOT NULL,
-  email VARCHAR NOT NULL,
-)
--- To join with users:
-  -- favorites: [
-  --   ObjectId(FAVORITE_ID) // all favorites ID for this user
-  -- ],
-  -- listings: [
-  --   ObjectId(LISTING_ID) // all listings ID for this user
-  -- ]
+_id INT NOT NULL PRIMARY KEY,
+username CHAR varying(30) NOT NULL,
+facebook VARCHAR,
+twitter VARCHAR,
+messenger VARCHAR,
+email VARCHAR,
+favorites VARCHAR,
+listings VARCHAR
+);
+
+CREATE TABLE listings (
+_id INT NOT NULL PRIMARY KEY,
+title CHAR varying(60) NOT NULL,
+location CHAR varying(60) NOT NULL,
+rating NUMERIC(2) NOT NULL,
+total_ratings INT NOT NULL,
+user_id INT NOT NULL REFERENCES users(_id),
+photos VARCHAR,
+favorites VARCHAR
+);
+
+CREATE TABLE photos (
+_id INT NOT NULL PRIMARY KEY,
+listing_id INT NOT NULL REFERENCES listings(_id),
+photo_url VARCHAR NOT NULL,
+priority INT NOT NULL,
+caption VARCHAR
+);
 
 CREATE TABLE favorites (
-  id INTEGER NOT NULL PRIMARY_KEY,
-  user_id INTEGER NOT NULL REFERENCES users(id),
-  listing_id INTEGER NOT NULL REFERENCES listings(id),
-  title VARCHAR NOT NULL
-)
+_id INT NOT NULL PRIMARY KEY,
+user_id INT NOT NULL REFERENCES users(_id),
+listing_id INT NOT NULL REFERENCES listings(_id),
+title VARCHAR
+);
+
+COPY users(_id,username,facebook,twitter,messenger,email,favorites,listings) FROM '/Users/eline/Desktop/photo-carousel-service/server/csvFiles/users.csv' DELIMITER ',' CSV HEADER;
+COPY listings(_id,title,location,rating,total_ratings,user_id,photos,favorites) FROM '/Users/eline/Desktop/photo-carousel-service/server/csvFiles/listings.csv' DELIMITER ',' CSV HEADER;
+COPY photos(_id,listing_id,photo_url,priority,caption) FROM '/Users/eline/Desktop/photo-carousel-service/server/csvFiles/photos.csv' DELIMITER ',' CSV HEADER;
+COPY favorites(_id,user_id,listing_id,title) FROM '/Users/eline/Desktop/photo-carousel-service/server/csvFiles/favorites.csv' DELIMITER ',' CSV HEADER;
