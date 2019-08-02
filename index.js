f9a66e9daa9cf11d047e7024b39a7a0a91e8a8f7
Elinex <eline.ax@gmail.com>
2019-08-02 15:07:07 -0700

    connect postgres with express server

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const expressStaticGzip = require('express-static-gzip');
 const { Op } = require('sequelize');
-const db = require('./server/db/index.js');
+// const db = require('./server/db/index.js');
+const db = require('./server/postgres/index.js');
 
 const app = express();
 const port = 3002;
@@ -36,76 +37,76 @@ app.use('/',express.static(path.resolve(__dirname, './public/dist')));
 app.use('/photoCarousel/:listingID', express.static(path.resolve(__dirname, './public/dist')));
 
 
-app.get('/api/listings/info/:listingID', (req, res) => {
-  const { listingID } = req.params;
+// app.get('/api/listings/info/:listingID', (req, res) => {
+//   const { listingID } = req.params;
 
-  db.Listing.findOne({
-    where: {
-      id: listingID,
-    },
-  }).then(result => res.send(result))
-    .catch(err => res.send(err));
-});
-
-
-app.get('/api/listings/photos/initial/:listingID', (req, res) => {
-  const { listingID } = req.params;
-
-  db.Photo.findAll({
-    where: {
-      listing_id: listingID,
-      priority: {
-        [Op.lte]: 4,
-      },
-    },
-    order: [
-      ['priority', 'ASC'],
-    ],
-  }).then(result => res.send(result))
-    .catch(err => res.send(err));
-});
-
-app.get('/api/listings/photos/:listingID', (req, res) => {
-  const { listingID } = req.params;
-
-  db.Photo.findAll({
-    where: {
-      listing_id: listingID,
-      priority: {
-        [Op.gte]: 5,
-      },
-    },
-    order: [
-      ['priority', 'ASC'],
-    ],
-  }).then(result => res.send(result))
-    .catch(err => res.send(err));
-});
+//   db.Listing.findOne({
+//     where: {
+//       id: listingID,
+//     },
+//   }).then(result => res.send(result))
+//     .catch(err => res.send(err));
+// });
 
 
-app.get('/api/users/:userID', (req, res) => {
-  const { userID } = req.params;
+// app.get('/api/listings/photos/initial/:listingID', (req, res) => {
+//   const { listingID } = req.params;
 
-  db.User.findOne({
-    where: {
-      id: userID,
-    },
-  }).then(result => res.send(result))
-    .catch(err => res.send(err));
-});
+//   db.Photo.findAll({
+//     where: {
+//       listing_id: listingID,
+//       priority: {
+//         [Op.lte]: 4,
+//       },
+//     },
+//     order: [
+//       ['priority', 'ASC'],
+//     ],
+//   }).then(result => res.send(result))
+//     .catch(err => res.send(err));
+// });
+
+// app.get('/api/listings/photos/:listingID', (req, res) => {
+//   const { listingID } = req.params;
+
+//   db.Photo.findAll({
+//     where: {
+//       listing_id: listingID,
+//       priority: {
+//         [Op.gte]: 5,
+//       },
+//     },
+//     order: [
+//       ['priority', 'ASC'],
+//     ],
+//   }).then(result => res.send(result))
+//     .catch(err => res.send(err));
+// });
 
 
-app.get('/api/users/lists/:userID/:listTitle', (req, res) => {
-  const { userID, listTitle } = req.params;
+// app.get('/api/users/:userID', (req, res) => {
+//   const { userID } = req.params;
 
-  db.List.findAll({
-    where: {
-      user_id: userID,
-      title: listTitle,
-    },
-  }).then(result => res.send(result))
-    .catch(err => res.send(err));
-});
+//   db.User.findOne({
+//     where: {
+//       id: userID,
+//     },
+//   }).then(result => res.send(result))
+//     .catch(err => res.send(err));
+// });
+
+
+// app.get('/api/users/lists/:userID/:listTitle', (req, res) => {
+//   const { userID, listTitle } = req.params;
+
+//   db.List.findAll({
+//     where: {
+//       user_id: userID,
+//       title: listTitle,
+//     },
+//   }).then(result => res.send(result))
+//     .catch(err => res.send(err));
+// });
 
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
