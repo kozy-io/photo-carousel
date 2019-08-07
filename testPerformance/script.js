699bc4efc505b03b1993d65c7c097542622c7c37
Elinex <eline.ax@gmail.com>
2019-08-07 15:41:00 -0700

    change test performance script

--- testPerformance/script.js
+++ testPerformance/script.js
@@ -4,30 +4,26 @@ const host = 'ec2-3-15-162-51.us-east-2.compute.amazonaws.com';
 
 const getListingUrl = listingId => `http://${host}:3002/api/listings/${listingId}`;
 // const getPhotosUrl = listingId => `http://${host}:3002/api/photos/${listingId}`;
-const id = () => Math.floor(Math.random() * Math.floor(10000000)) | 1;
 
-const getListing = () => {
-  var res = http.get(getListingUrl(id()));
+const getListing = (id) => {
+  var res = http.get(getListingUrl(id));
   // console.log("GET LISTING response time was " + String(res.timings.duration) + " ms");
 };
 
 // SINCE IMAGES ARE NOT IN POSTGRES EC2, I'LL NOT MAKE TESTS WITH IT NOW
 
-// const getPhotos = () => {
-//   var res = http.get(getPhotosUrl(id()));
+// const getPhotos = (id) => {
+//   var res = http.get(getPhotosUrl(id));
 //   // console.log("GET PHOTOS response time was " + String(res.timings.duration) + " ms");
 // };
 
 export default function() {
-  getListing();
+  const id = Math.floor(Math.random() * Math.floor(10000000)) | 1;
+  getListing(id);
 }; 
 
 // export default function() {
-//   const request = Math.floor(Math.random() * Math.floor(2));
-//   if (request === 0){
-//     getListing();
-//   }
-//   else {
-//     getPhotos();
-//   }
+  // const id = Math.floor(Math.random() * Math.floor(10000000)) | 1;
+  // getListing(id);
+  // getPhotos(id);
 // }; 
